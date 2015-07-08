16241eb4c5e933ef010ee43f5d062a38d00a0504
Casy <el.casy@gmail.com>
2015-07-08 17:22:49 -0300

    CSS little fix

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,21 @@
 module.exports = function (grunt) {
-              grunt.initConfig({
-                watch:{
-                  scripts:{
-                    files:['fuente/*.js'],
-                    tasks: ['browserify'],
-                  }
-                  },
-                  browserify:{
-                    client: {
-                      src: ['fuente/*.js'],
-                      dest: 'js/bundle.js',
-                    }
-                  }
-                  });
-                  
-                  grunt.loadNpmTasks('grunt-contrib-watch');
-                  grunt.loadNpmTasks('grunt-browserify');
-                  
-                  grunt.registerTask('default',['watch']);
-                  }
+  grunt.initConfig({
+    watch:{
+      scripts:{
+        files:['fuente/*.js'],
+        tasks: ['browserify'],
+      }
+    },
+    browserify:{
+      client: {
+        src: ['fuente/*.js'],
+        dest: 'js/bundle.js',
+      }
+    }
+  });
+  
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browserify');
+  
+  grunt.registerTask('default',['watch']);
+}
