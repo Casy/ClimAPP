d2f59f0ba305a1be29a1f1ef631aa20730619304
gonzaldd <gonzalodedomingo@gmail.com>
2015-07-13 12:10:00 -0300

    .

--- js/bundle.js
+++ js/bundle.js
@@ -1920,13 +1920,13 @@ var setCercanos = function setCercanos(datos){
 	$('#minima_dia_1').html(parseInt(datos.clima_completo.list[0].temp.min,10));
 	/////////////////////
 
-	for(i=0; i <2; i++){
+	for(i=1; i <=2; i++){
 		$('#localidad_cercania_'+(i+1)).html(datos.ciudades_cercanas.list[i].name);
 		$('#img_cercania_'+(i+1)).attr('src','img/ICONS/'+datos.ciudades_cercanas.list[i].weather[0].icon+'.png');
 		$('#temperatura_cercania_'+(i+1)).html(parseInt(datos.ciudades_cercanas.list[0].main.temp,10));	
 	}
 }
-
+ 
 
 
 module.exports = setCercanos;
